03848e2c896cd6e456586546dc8436c5d0284abc
DestinyFeng <1099773854@qq.com>
2019-09-11 16:32:35 +0800

    跟新电影模块

--- movie-stage/src/main/java/com/woniu/api/MovieAPI.java
+++ movie-stage/src/main/java/com/woniu/api/MovieAPI.java
@@ -1,7 +1,7 @@
 package com.woniu.api;
 
-import com.woniu.service.MovieService;
 import com.woniu.util.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ import javax.annotation.Resource;
 public class MovieAPI {
     @Resource
     private MovieService movieService;
-    @RequestMapping
+    @GetMapping
     public Result selectMovie(Integer mid) throws Exception{
         System.out.println(mid);
         return new Result("success",null,null,movieService.selectMovie(mid));
