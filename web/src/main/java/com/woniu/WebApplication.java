883f7ad3a8803e07ac55eb80ef4ea834d53cee30
taoran <1153568377@qq.com>
2019-09-17 20:55:56 +0800

    评论模块

--- web/src/main/java/com/woniu/WebApplication.java
+++ web/src/main/java/com/woniu/WebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
     public static void main(String[] args) {
+        System.out.println("holleo word");
         SpringApplication.run(WebApplication.class, args);
     }
 
